ae4da1e6a20b7a18c27a64f552c9ea4701d7e3ae
Dmajor81 <christopherleon237@gmail.com>
2023-09-23 19:37:53 +0100

    unmatched url error handled

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express, { Application, Request, Response } from "express";
+import express, { Application, NextFunction, Request, Response } from "express";
 import droneRoutes from "./routes/drones.routes";
 import medicationRoutes from "./routes/medications.routes";
 import { globalErrorHandler } from "./middleware/error.middleware";
@@ -11,7 +11,15 @@ app.get("/", (req: Request, res: Response) => {
 });
 app.use("/drones", droneRoutes);
 app.use("/medications", medicationRoutes);
+app.all("*", (req: Request, res: Response, next: NextFunction) => {
+  const error = new Error(`Can't find ${req.originalUrl} on this server!`);
+  res.status(404).json({
+    error: {
+      message: error.message,
+    },
+  });
+});
 
-app.use(globalErrorHandler)
+app.use(globalErrorHandler);
 
 export default app;
