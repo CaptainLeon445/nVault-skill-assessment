970bcc563c26e8bac544f8c9a162f68a6aca95c5
Dmajor81 <christopherleon237@gmail.com>
2023-09-24 09:51:42 +0100

    fixed bugs and updated the code

--- src/logger.ts
+++ src/logger.ts
@@ -1,23 +1,8 @@
 import winston from "winston";
-const { combine, timestamp, printf, colorize, align } = winston.format;
+const { combine, timestamp, json } = winston.format;
 
-const logLevels = {
-    fatal: 0,
-    error: 1,
-    warn: 2,
-    info: 3,
-    debug: 4,
-    audit: 5,
-  };
 const logger = winston.createLogger({
-    format: combine(
-        // colorize({ all: true }),
-        timestamp({
-          format: 'YYYY-MM-DD hh:mm:ss.SSS A',
-        }),
-        // align(),
-        printf((info) => `[${info.timestamp}] ${info.level}: ${info.message}`)
-      ),
+  format: combine(timestamp(), json()),
     transports: [
         new winston.transports.Console,
         new winston.transports.File({ filename: 'logs/error/error.log', level: 'error' }),
--- src/scheduler/checkBatteryLevels.ts
+++ src/scheduler/checkBatteryLevels.ts
@@ -3,21 +3,27 @@ import logger from "../logger";
 import DroneService from "../services/drone.service";
 
 class job {
-  constructor() {}
 
-  //Check Battery level of the drones every minute
-  public async checkBatteryLevel() {
-    var rule = new schedule.RecurrenceRule();
-    rule.second = 0;
-    schedule.scheduleJob(rule, async function () {
-      try {
-        const data = await DroneService.checkDroneBatteryLevels();
-        logger.info(data);
-      } catch (error) {
-        logger.error("Error in checking Battery Levels");
-      }
-    });
-  }
+    constructor() {
+
+    }
+    
+    //Check Battery level of the drones every minute
+    public async checkBatteryLevel() {
+        var rule = new schedule.RecurrenceRule();
+        rule.second = 0;
+        schedule.scheduleJob(rule, async function() {
+            try{
+                const data=await DroneService.checkDroneBatteryLevels()
+                console.log(data)
+                logger.info({
+                    batteryLevels: data
+                })
+            }catch(error){
+                logger.error("Error in checking Battery Levels")
+            }
+        })
+    }
 }
 
 export default new job();
